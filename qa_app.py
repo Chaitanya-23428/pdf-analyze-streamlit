8323b862cd1016476808f85490b3215cc5451e28
Chaitanya-23428 <130089617+Chaitanya-23428@users.noreply.github.com>
2023-10-10 13:55:20 +0530

    Update qa_app.py

--- qa_app.py
+++ qa_app.py
@@ -113,7 +113,7 @@ def main():
         padding: 0px 0px;
         text-align: center;
     ">
-        <p>Made by <a href='https://twitter.com/mehmet_ba7'>THE TECH WHIZZES</a></p>
+        <p>Made by <a href='https://x.com/amulyaeamani'>THE TECH WHIZZES</a></p>
     </div>
     """
 
